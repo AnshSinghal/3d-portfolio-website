c3eb47d114d9e95bc45c23eafd1b0a1793b76a99
Ansh Singhal <e23cseu0145@bennett.edu.in>
2024-02-26 17:05:04 +0530

    changes made to smoothly load the 3d Components

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,20 +1,20 @@
 import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";  //importing Canvas from react-three-fiber
 import {
   Decal,
   Float,
   OrbitControls,
   Preload,
   useTexture,
-} from "@react-three/drei";
+} from "@react-three/drei";       //importing Decal, Float, OrbitControls, Preload, useTexture from react-three-drei
 
-import CanvasLoader from "../Loader";
+import CanvasLoader from "../Loader";   //importing CanvasLoader from Loader
 
-const Ball = (props) => {
-  const [decal] = useTexture([props.imgUrl]);
+const Ball = (props) => {   //creating a Ball component
+  const [decal] = useTexture([props.imgUrl]);   //using useTexture to load the image of the ball
 
   return (
-    <Float speed={1.75} rotationIntensity={2} floatIntensity={4}>
+    <Float speed={1.75} rotationIntensity={2} floatIntensity={4}>   //using Float to make the ball float
       <ambientLight intensity={2} />
       <directionalLight position={[0, 0, 0.15]} />
       <mesh castShadow receiveShadow scale={2.75}>
